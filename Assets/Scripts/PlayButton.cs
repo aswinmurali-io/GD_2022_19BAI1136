866679ae50429ae3bd2223b2cc2c44d99fbf7cd2
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-09-11 10:32:53 +0530

    completes level 3

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        var play = yourButton.GetComponent<Button>();
+        play.onClick.AddListener(MoveToGame);
+    }
+
+    void MoveToGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
