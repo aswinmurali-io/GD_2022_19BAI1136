5a047f3727a68ff1f53f93e5c6b27216272b20cc
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-09-11 10:23:16 +0530

    completes level 2

--- Assets/Scripts/DoofusScore.cs
+++ Assets/Scripts/DoofusScore.cs
@@ -1,14 +1,22 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class DoofusScore : MonoBehaviour
 {
+    private int[] pulpitsVisitedIds;
+
     public int score = 0;
 
     public TextMeshProUGUI scoreText;
 
+    void OnTriggerEnter(Collider crash)
+    {
+        Debug.Log(true);
+    }
+
     void Update()
     {
-        scoreText.text = System.String.Format("Score {0}", score);
+        scoreText.text = String.Format("Score {0}", score);
     }
 }
